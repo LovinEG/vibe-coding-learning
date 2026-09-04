f5e64e58e8fd800b52b672c5e9dd0164286b24fb
lovineg <fursae@gmail.com>
2026-09-05 02:34:00 +0300

    Add auth sign in

---
+++ src/lib/auth.js
@@ -0,0 +1,7 @@
+import { supabase } from './supabase'
+
+export async function signIn(email, password) {
+  const result = await supabase.auth.signInWithPassword({ email, password })
+
+  return result
+}
